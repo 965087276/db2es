066662f6a00a1c07ac7ad0d8a44e30a1331d8fe6
wangjinhao <965087276@qq.com>
2018-12-11 11:44:10 +0800

    使用JSONObject代替Object和Object[]

--- dbinfo/src/main/java/cn/dbinfo/service/secondary/ColumnResultService.java
+++ dbinfo/src/main/java/cn/dbinfo/service/secondary/ColumnResultService.java
@@ -1,10 +1,12 @@
 package cn.dbinfo.service.secondary;
 
 import cn.dbinfo.dao.secondary.ColumnResultDAO;
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,11 +14,24 @@ public class ColumnResultService {
     @Autowired
     private ColumnResultDAO columnResultDAO;
 
-    public List<Object[]> findByDatabaseAndTable(String database, String table) {
-        return columnResultDAO.findByDatabaseAndTable(database, table);
+//    public List<Object[]> findByDatabaseAndTable(String database, String table) {
+//        return columnResultDAO.findByDatabaseAndTable(database, table);
+//    }
+
+    public List<JSONObject> findByDatabaseAndTable(String database, String table) {
+        List<Object[]> infos = columnResultDAO.findByDatabaseAndTable(database, table);
+        List<JSONObject> columnAndTypes = new ArrayList<>();
+        for (Object[] info : infos) {
+            JSONObject columnAndType = new JSONObject();
+            columnAndType.put("column_name", info[0].toString());
+            columnAndType.put("column_type", info[1].toString());
+            columnAndTypes.add(columnAndType);
+        }
+        return columnAndTypes;
     }
 
-    public List<Object> findTableByDatabase(String database) {
-        return columnResultDAO.findTableByDatabase(database);
-    }
+//    public List<Object> findTableByDatabase(String database) {
+//        return columnResultDAO.findTableByDatabase(database);
+//    }
+
 }
